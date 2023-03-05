96e00e88d2780fcbd35fea9dc5179aabadb86836
student <student@M40312>
2023-03-06 08:56:59 +0900

    정올01

---
+++ 정올/src/J9063_배열1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class J9063_배열1 { //제출시 Main으로 수정
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int [] a = new int[5];
+		
+		for (int i = 0; i < 5; i++) {
+			a[i] = sc.nextInt();
+			
+		}
+		//출력
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+" ");
+		}
+	}
+
+}
